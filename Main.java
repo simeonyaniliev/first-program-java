a4f3ecf1eb8eb339069e87e29ed45274ed4cd463
Simeon Iliev <49561709+simeonyaniliev@users.noreply.github.com>
2019-05-30 22:42:21 +0200

    Improve lines printing in the console

--- Main.java
+++ Main.java
@@ -10,22 +10,22 @@ public class Main
 		// console execution paragraph starts
 		// -- "Wow" section
 		System.out.print("Enter something for a test:  ");
-	           String wowInput = System.console().readLine();
-	           System.out.print("Wow: " + wowInput);
+	        String wowInput = System.console().readLine();
+	        System.out.println("Wow: " + wowInput);
 		
 		// -- "Hi" section
-	           System.out.print("Enter your first name:  ");
-	           String firstName = System.console().readLine();
-	           System.out.print("Hi " + firstName);
+	        System.out.print("Enter your first name:  ");
+	        String firstName = System.console().readLine();
+	        System.out.println("Hi " + firstName);
 	
 		// -- "Authenticate" section
-	           System.out.print("To authenticate, enter your last name:  ");
-	           String lastName = System.console().readLine();
+	        System.out.print("To authenticate, enter your last name:  ");
+	        String lastName = System.console().readLine();
 	
 		If (lastName.equals("Iliev"))  {  // write your own family name here
-	               System.out.print("Regards boss: " + lastName);
+	        	System.out.println("Regards boss: " + lastName);
 		} else {
-		     System.out.print("Regards : " + lastName);
+		     	System.out.println("Regards : " + lastName);
 		}
 	
 		// console execution paragraph ends
